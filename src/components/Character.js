414a5530428a1450934605657f0f0fe91ac7fff1
Raymond Wu <raymonwu@umich.edu>
2021-12-26 17:25:29 -0500

    fixed errors in search

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
-const CHARACTER_IMG_API = 'https://api.genshin.dev/'
+//vvv idk if needed anymore vvv
+// const CHARACTER_IMG_API = 'https://api.genshin.dev/'
 
 const Character = (name, weapon, vision) => {
     return (
