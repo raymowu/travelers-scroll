e801a12f638f375c335a50e826f7ef92cd40fed6
kakandikonda <KaKandikondak@hotmail.com>
2022-01-13 17:15:58 -0500

    Created a router
    This cleans up the server page so that our routes and our server set up things are separated.

--- Server/app.js
+++ Server/app.js
@@ -40,67 +40,13 @@ const Authenticate = (req, res, next) => {
 	else{
 	  next()
 	}
-  }
+}
 
 // Routes
-app.get("/", (req, res) => {
-	res.redirect("/blog");
-})
+const indexRoutes = require("./routes/index.js");
 
-app.post("/register", async (req, res) => {
-	const { username, password } = req.body;
-	User.findOne({ username}, async (err, user) => {
-		if (err) throw err;
-		if (user) res.send({status: "err", message: "User Already Exists"});
-		if (!user){
-			const hashed = await bcrypt.hash(password, 10);
-			const newUser = new User({
-				username,
-				password: hashed
-			});
-			await newUser.save()
-			req.session.user = req.session.user = {id: newUser._id, username: newUser.username};
-			res.send({status: "ok"})
-		}
-	})
-});
+app.use(indexRoutes)
 
-app.post("/login", async (req, res) => {
-	const { username, password } = req.body;
-	const user = await User.findOne({ username });
-	if(user){
-		const valid = await bcrypt.compare(password, user.password);
-		if(valid){
-			req.session.user = {id: user._id, username: user.username};
-			res.send({status: "ok"});
-		}
-	}
-})
-
-app.get("/current-user", Authenticate, (req, res) => {
-	res.send({status: "ok", user: req.session.user});
-});
-
-app.get("/profile/:id", (req, res) => {
-	User.findById({_id: req.params.id}, async (err, user) => {
-		if(err){
-			res.send({status: "err"})
-		}
-		if(!user){
-			res.send({status: "err", message: "Unable to find user"})
-		}
-		else{
-			res.send({status: "ok", user: {username: user.username}})
-		}
-	})
-})
-
-
-// logout rout
-app.get("/logout", (req, res) => {
-	req.logout();
-	res.send({status: "ok", message: "Successfully logged out"})
-});
 
 
 app.listen(5000, () =>{{
---
+++ Server/routes/index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs")
+const User = require("../models/user");
+
+const Authenticate = (req, res, next) => {
+	if(!req.session.user){
+	  res.send({status: "err", message: "Login Required"});
+	}
+	else{
+	  next()
+	}
+}
+
+router.get("/", (req, res) => {
+    res.send({status: "ok"})
+})
+
+router.post("/register", async (req, res) => {
+	const { username, password } = req.body;
+	User.findOne({ username}, async (err, user) => {
+		if (err) throw err;
+		if (user) res.send({status: "err", message: "User Already Exists"});
+		if (!user){
+			const hashed = await bcrypt.hash(password, 10);
+			const newUser = new User({
+				username,
+				password: hashed
+			});
+			await newUser.save()
+			req.session.user = req.session.user = {id: newUser._id, username: newUser.username};
+			res.send({status: "ok"})
+		}
+	})
+});
+
+router.post("/login", async (req, res) => {
+	const { username, password } = req.body;
+	const user = await User.findOne({ username });
+	if(user){
+		const valid = await bcrypt.compare(password, user.password);
+		if(valid){
+			req.session.user = {id: user._id, username: user.username};
+			res.send({status: "ok"});
+		}
+	}
+})
+
+router.get("/current-user", Authenticate, (req, res) => {
+	res.send({status: "ok", user: req.session.user});
+});
+
+// logout rout
+router.get("/logout", (req, res) => {
+	req.session.destroy();
+});
+
+router.get("/profile/:id", (req, res) => {
+	User.findOne({username: req.params.id}, async (err, user) => {
+		if(err){
+			res.send({status: "err"})
+		}
+		if(!user){
+			res.send({status: "err", message: "Unable to find user"})
+		}
+		else{
+			res.send({status: "ok", user: {username: user.username}})
+		}
+	})
+})
+
+
+module.exports = router;
