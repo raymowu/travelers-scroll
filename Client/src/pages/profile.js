095ac92603e01911d63d99e0f52ffbad4f6db4fd
kakandikonda <KaKandikondak@hotmail.com>
2022-01-16 19:33:31 -0500

    Made some user changes
    Can Logout now, and users can view options to edit/delete their account.

--- Client/src/pages/profile.js
+++ Client/src/pages/profile.js
@@ -6,25 +6,52 @@ import Layout from "../components/Layout"
 function Profile() {
 
     const {id} = useParams();
+    const [modifier, setModifier] = useState(false)
     const [user, setUser] = useState({})
 
     useEffect(() =>{
-        Axios.get(`http://localhost:5000/profile/${id}`).then((response) => {
+        Axios.get(`http://localhost:5000/profile/${id}`, {withCredentials: true}).then((response) => {
           console.log(response.data)
           // console.log(response.data.blogs)
           if (response.data.status === "ok"){
-              setUser(response.data.user)
+              setUser(response.data.user);
+              if(response.data.modifier){
+                  setModifier(true);
+              }
           }
           else{
               alert(response.data.message)
           }
         });
       }, []);
+
+      const Logout = () => {
+        Axios.get(`http://localhost:5000/logout`, {withCredentials: true}).then((response) => {
+          console.log(response.data)
+          // console.log(response.data.blogs)
+          if (response.data.status === "ok"){
+            alert("Logout was successful");
+          }
+          else{
+              alert(response.data.message)
+          }
+        });
+      }
     return(
         <Layout Auth={false}>
             <div>
                 <h1>profile page for {user.username}</h1>
             </div>
+            {modifier 
+            ?
+            <>
+            <div>edit, delete</div> 
+            <button onClick={Logout}><a href="/">Logout</a></button>
+            </>
+            
+            :
+            <></>
+            }
         </Layout>
         
     )
