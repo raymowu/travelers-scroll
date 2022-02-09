fe44d76a17204a8bc94b2842b795132cfc8c3618
Raymond Wu <raymonwu@umich.edu>
2022-02-09 00:00:14 -0500

    new character header backdrop

--- Client/src/pages/Character.js
+++ Client/src/pages/Character.js
@@ -1,67 +1,87 @@
-import React from 'react'
-import '../css/character.css'
-import { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import Layout from "../components/Layout"
-const CHARACTER_API = 'https://api.genshin.dev/characters/';
+import React from "react";
+import "../css/character.css";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Layout from "../components/Layout";
+const CHARACTER_API = "https://api.genshin.dev/characters/";
 
 const Character = () => {
+  const [character, setCharacter] = useState([]);
+  useEffect(() => {
+    getCharacter(characterName);
+  }, []);
 
-    const [ character, setCharacter ] = useState([]);
-    useEffect(() => {
-        getCharacter(characterName);
-        }, []);
-    
-    
-        //fetches character w unique character api
-        const getCharacter = (characterName) => {
-        fetch(CHARACTER_API + characterName)
-        .then(res => res.json())
-        .then(data => {
-            setCharacter(data);
-        })
-        // const res = await fetch(API);
-        // const data = await res.json()
-        // setCharacter(data.results)
-        }
+  //fetches character w unique character api
+  const getCharacter = (characterName) => {
+    fetch(CHARACTER_API + characterName)
+      .then((res) => res.json())
+      .then((data) => {
+        setCharacter(data);
+      });
+    // const res = await fetch(API);
+    // const data = await res.json()
+    // setCharacter(data.results)
+  };
 
-    let { characterName } = useParams();
-    
-    
+  let { characterName } = useParams();
 
-    return (
-        <Layout>
-            <div className="container">
-                <div className={`character-header ${characterName}`}
-                style={{backgroundImage: `url(https://api.genshin.dev/characters/${characterName}/gacha-splash)`}}>
-                    <img src={(CHARACTER_API + characterName + '/icon')} alt={characterName} />
-                    <ul>
-                        <li><h2>Genshin Impact</h2></li>
-                        <li><h1>{character.name}</h1></li>
-                        <li><h2> <span className={`${character.vision}`}>{character.vision}</span> &#x2022; {character.weapon}</h2></li>
-                    </ul>
-                
-                </div>
-                
-                <div className="character-navbar">
-                    <ul>
-  <li><a href="#">Description</a></li>
-  <li><a href="#">Weapon</a></li>
-  <li><a href="#">Artifacts</a></li>
-  <li><a href="#">Teams</a></li>
-  <li><a href="#">Showcase</a></li>
-</ul>
-                    </div>
+  return (
+    <Layout>
+      <div className="container">
+        <div
+          className={`character-header ${characterName}`}
+          style={{
+            backgroundImage: `url(https://api.genshin.dev/characters/${characterName}/gacha-splash)`,
+          }}
+        >
+          <img
+            src={CHARACTER_API + characterName + "/icon"}
+            alt={characterName}
+          />
+          <ul>
+            <li>
+              <h2>Genshin Impact</h2>
+            </li>
+            <li>
+              <h1>{character.name}</h1>
+            </li>
+            <li>
+              <h2>
+                {" "}
+                <span className={`${character.vision}`}>
+                  {character.vision}
+                </span>{" "}
+                &#x2022; {character.weapon}
+              </h2>
+            </li>
+          </ul>
+        </div>
 
-                <h3>{character.description}</h3>
-                    
-                
-            </div>
-        </Layout>
-        
-        
-                  
-    )
-}
+        <div className="character-navbar">
+          <ul>
+            <li>
+              <a href="#">Description</a>
+            </li>
+            <li>
+              <a href="#">Weapon</a>
+            </li>
+            <li>
+              <a href="#">Artifacts</a>
+            </li>
+            <li>
+              <a href="#">Teams</a>
+            </li>
+            <li>
+              <a href="#">Showcase</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="container2">
+        <h3>{character.description}</h3>
+      </div>
+    </Layout>
+  );
+};
 
-export default Character
+export default Character;
