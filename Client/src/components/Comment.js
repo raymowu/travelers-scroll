354bee6c1d28890ecd5139264a700cf0a305966c
Raymond Wu <raymonwu@umich.edu>
2022-05-07 21:28:36 -0400

    Allowed for longer comments to wrap correctly

--- Client/src/components/Comment.js
+++ Client/src/components/Comment.js
@@ -4,8 +4,9 @@ const Comment = ({ comment }) => {
   return (
     <div className="comment">
       <h2 className="comment-username">{comment.Author.username}</h2>
-
+      <div className="break"></div>
       <h3 className="comment-text">{comment.text} </h3>
+      <div className="break"></div>
     </div>
   );
 };
