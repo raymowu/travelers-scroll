a7834176d8a2c17110c655bc9e3bf21549390412
Raymond Wu <raymonwu@umich.edu>
2022-06-01 06:46:29 -0400

    fixd artifact display positioning

--- Client/src/components/CharacterCard.js
+++ Client/src/components/CharacterCard.js
@@ -8,7 +8,15 @@ const CharacterCard = ({ characterName }) => {
     <>
       <div className="character">
         <a href={`/characters/${characterName}`}>
-          <img src={CHARACTER_API + characterName + "/icon"} alt={characterName} />
+          <img
+            src={CHARACTER_API + characterName + "/icon"}
+            onError={(e) => {
+              e.target.onerror = null;
+              e.target.src =
+                "https://preview.redd.it/9bcv6smhvx961.png?width=640&crop=smart&auto=webp&s=f5b7d40d216a89b1d8a4c77a41dff92a5431980b";
+            }}
+            alt={characterName}
+          />
         </a>
         <h3>{initializeName(characterName)}</h3>
       </div>
