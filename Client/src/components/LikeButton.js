f0b387101280a0aeee56ae5298cbc33b86a6ecfa
Raymond Wu <raymonwu@umich.edu>
2022-06-03 17:06:28 -0400

    Made like button design

--- Client/src/components/LikeButton.js
+++ Client/src/components/LikeButton.js
@@ -2,7 +2,7 @@ import { FaRegThumbsUp, FaThumbsUp } from "react-icons/fa";
 
 const LikeButton = ({ liked, handleOnLike }) => {
   if (liked) {
-    return <FaThumbsUp className="like-button" onClick={handleOnLike} />;
+    return <FaThumbsUp className="like-button-liked" onClick={handleOnLike} />;
   } else {
     return <FaRegThumbsUp className="like-button" onClick={handleOnLike} />;
   }
