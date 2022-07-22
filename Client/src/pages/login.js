06c12c59e7746e7d708e8fc2f890dcb676487554
Raymond Wu <raymonwu@umich.edu>
2022-07-22 18:19:36 -0400

    Fixed tag error

--- Client/src/pages/login.js
+++ Client/src/pages/login.js
@@ -1,27 +1,28 @@
 import { useState, useEffect } from "react";
 import Axios from "axios";
 import "../css/form.css";
-import { gapi } from "gapi-script"
+import { gapi } from "gapi-script";
 import { GoogleLogin } from "react-google-login";
 
 function LogIn() {
   const [form, setForm] = useState({ username: "", password: "" });
-  const ClientId = "807573379511-9us9tvqh79lupajoa0mnv91r2c6g2lml.apps.googleusercontent.com";
+  const ClientId =
+    "807573379511-9us9tvqh79lupajoa0mnv91r2c6g2lml.apps.googleusercontent.com";
 
   useEffect(() => {
     function start() {
       gapi.client.init({
         clientId: ClientId,
-        scope: ""
+        scope: "",
       });
     }
-    gapi.load('client:auth2', start);
-  })
+    gapi.load("client:auth2", start);
+  });
   const googleAuth = ({ profileObj }) => {
-    const data =  {
+    const data = {
       gid: profileObj.googleId,
-      email: profileObj.email
-    }
+      email: profileObj.email,
+    };
     // console.log(typeof data.gid);
     Axios({
       method: "POST",
@@ -32,17 +33,15 @@ function LogIn() {
       if (res.data.status === "ok") {
         alert("Login was successful");
         window.location.href = "/";
-      }
-      else if(res.data.status === "err"){
+      } else if (res.data.status === "err") {
         alert(res.data.message);
       }
     });
-    
   };
   const failure = (error) => {
     console.log("err", error);
     console.log("failed");
-  }
+  };
 
   async function login(event) {
     event.preventDefault();
@@ -95,25 +94,23 @@ function LogIn() {
 
         <div>
           <GoogleLogin
-          clientId= {ClientId}
-          onSuccess={googleAuth}
-          onFailure={failure}
-          cookiePolicy={"single_host_origin"}
-          className="googlebtn"
-        >
-          <span>Sign in with Google</span>
-        </GoogleLogin>
+            clientId={ClientId}
+            onSuccess={googleAuth}
+            onFailure={failure}
+            cookiePolicy={"single_host_origin"}
+            className="googlebtn"
+          >
+            <span>Sign in with Google</span>
+          </GoogleLogin>
         </div>
         <br />
-        <p>
 
         <p className="sign-up-text">
-
           Dont have an account? <a href="/register">Sign Up</a>
         </p>
-        <p>
-          <a href="/forgotpassword">Forgot Password?</a>
-        </p>
+
+        <a href="/forgotpassword">Forgot Password?</a>
+
         <a href="/">Back</a>
       </form>
     </div>
