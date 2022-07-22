e7f84e1f5a83d42129ab8308c66bfe4c7bde0f25
Raymond Wu <raymonwu@umich.edu>
2022-07-22 18:27:02 -0400

    Fixed login and register page alignments

--- Client/src/pages/signup.js
+++ Client/src/pages/signup.js
@@ -1,27 +1,28 @@
 import { useState, useEffect } from "react";
 import "../css/form.css";
 import Axios from "axios";
-import { gapi } from "gapi-script"
+import { gapi } from "gapi-script";
 import { GoogleLogin } from "react-google-login";
 
 function SignUp() {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
-  const ClientId = "807573379511-9us9tvqh79lupajoa0mnv91r2c6g2lml.apps.googleusercontent.com";
+  const ClientId =
+    "807573379511-9us9tvqh79lupajoa0mnv91r2c6g2lml.apps.googleusercontent.com";
 
   useEffect(() => {
     function start() {
       gapi.client.init({
         clientId: ClientId,
-        scope: ""
+        scope: "",
       });
     }
-    gapi.load('client:auth2', start);
-  })
+    gapi.load("client:auth2", start);
+  });
   const googleAuth = ({ profileObj }) => {
-    const data =  {
+    const data = {
       gid: profileObj.googleId,
-      email: profileObj.email
-    }
+      email: profileObj.email,
+    };
     // console.log(typeof data.gid);
     Axios({
       method: "POST",
@@ -32,17 +33,15 @@ function SignUp() {
       if (res.data.status === "ok") {
         alert("Account has been successfully created!");
         window.location.href = "/";
-      }
-      else if(res.data.status === "err"){
+      } else if (res.data.status === "err") {
         alert(res.data.message);
       }
     });
-    
   };
   const failure = (error) => {
     console.log("err", error);
     console.log("failed");
-  }
+  };
 
   async function register(event) {
     event.preventDefault();
@@ -100,26 +99,22 @@ function SignUp() {
         <button type="submit" className="button-form">
           Submit
         </button>
-        
         <div>
           <GoogleLogin
-          clientId= {ClientId}
-          onSuccess={googleAuth}
-          onFailure={failure}
-          cookiePolicy={"single_host_origin"}
-          className="googlebtn"
-        >
-          <span>Sign Up with Google</span>
-        </GoogleLogin>
+            clientId={ClientId}
+            onSuccess={googleAuth}
+            onFailure={failure}
+            cookiePolicy={"single_host_origin"}
+            className="googlebtn"
+          >
+            <span>Sign Up with Google</span>
+          </GoogleLogin>
         </div>
-        <p>
+        <p className="already-have-an-account">
           Already have an account? <a href="/login">Login</a>
         </p>
         <a href="/">Back</a>
-        
       </form>
-      
-      
     </div>
   );
 }
