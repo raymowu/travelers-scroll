0a1f6aa459ced87c19b632f3d4c98da3e5a875ca
kakandikonda <KaKandikondak@hotmail.com>
2022-11-08 14:55:55 -0500

    fixing cors

--- Client/src/pages/CreateBuild.js
+++ Client/src/pages/CreateBuild.js
@@ -158,7 +158,7 @@ const CreateBuild = () => {
         },
         withCredentials: true,
         url: "https://travelerscroll.herokuapp.com/builds",
-      }).then((res) => {
+      }, {withCredentials:true}).then((res) => {
         if (res.data.status === "err") {
           console.log(res.data);
           alert("LOGIN REQUIRED");
